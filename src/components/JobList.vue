4d51f6dac6ec1bebbee1b30e01a22d59123b57fd
Yared <yaredyh11@gmail.com>
2024-11-29 11:52:25 +0300

    search functionality added

--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="job-list">
+      <div class="top-nav">
         <p>Ordered by {{ order }}</p>
+        <input type="text" placeholder="Search by title..." class="searchbox" v-model="searchInp">
+      </div>
         <transition-group name="list" tag="ul">
-            <li v-for="job in orderedJob" :key="job.id">
+            <li v-for="job in filteredJob" :key="job.id">
                 <h1>{{ job.title }} in {{ job.location }}</h1>
                 <div class="salary">
                     <p>{{ job.salary }} Birr</p>
@@ -16,7 +19,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, PropType } from 'vue'
+import { computed, defineComponent, PropType, ref } from 'vue'
 import Job from '@/types/Job'
 import OrderType from '@/types/OrderType';
 
@@ -32,12 +35,21 @@ export default defineComponent({
         }
     },
     setup(props){
-        const orderedJob = computed(()=>{
-            return [...props.jobs].sort((a,b)=>{
-                return a[props.order] > b[props.order] ? 1 : -1
-            })
+      const searchInp = ref('')
+
+      const filteredJob = computed(()=>{
+        const filterJob = searchInp.value.length > 0 ? [...props.jobs].filter((jobTitle)=>{
+          return jobTitle.title.toLowerCase().includes(searchInp.value.toLowerCase())
+        }): [...props.jobs]
+
+        return filterJob.sort((a,b)=>{
+          return a[props.order] > b[props.order] ? 1 : -1
         })
-        return { orderedJob }
+
+      })
+      
+      
+        return { filteredJob, searchInp }
     }
 })
 </script>
@@ -74,5 +86,21 @@ export default defineComponent({
   }
   .list-move{
     transition: all 1s;
+  }
+  .top-nav{
+    display: flex;
+    justify-content: space-between;
+  }
+  .top-nav input{
+    padding: 8px 10px !important;
+    width: 250px;
+    height: 25px;
+    border: 1px solid #cacaca;
+    border-radius: 8px;
+    font-size: 16px;
+  }
+  .top-nav input:focus{
+    outline: none;
+    border: 1px solid #cacaca;
   }
 </style>
